355858069dae27dbba8607e1fc8462576f358ef3
gxavier <gxavier38@gmail.com>
2019-04-03 09:38:00 -0400

    Add fitness function

---
+++ getFit.m
@@ -0,0 +1,33 @@
+function [fitness] = getFit(C, X, Y, L, Sx, Sy)
+%GETFIT Calculates the maxLoad/cost ratio of a truss
+fitness = -1;
+
+cost = getCost(C,X,Y);
+lengthMatrix = getLengthMatrix(C,X,Y);
+
+correct = checkValid(C,X,Y,cost,lengthMatrix);
+if (~correct)
+    return;
+end
+
+T = getForces(C,X,Y,L,Sx,Sy,lengthMatrix);
+
+[~,numMembers] = size(C);
+
+% Failure analysis
+comp = zeros(numMembers,1);
+for i = 1:numMembers
+   if (T(i) > 0)
+       comp(i) = inf;
+       continue;
+   end
+   comp(i) = -T(i);
+end
+
+bucklingForces = getBucklingForces(lengthMatrix);
+scalingRatio = bucklingForces ./ comp;
+[maxSR,~] = max(scalingRatio);
+maxLoad = 1 / maxSR;
+fitness = maxLoad/cost;
+end
+
